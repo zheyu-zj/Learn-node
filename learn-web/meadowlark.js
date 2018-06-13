a8a19e2c4d68f4d84709464f6272ca3e3f60cd3e
zhaojie <zhaojie@123.com>
2018-06-14 00:01:23 +0800

    创建基础模板+express路由

--- learn-web/meadowlark.js
+++ learn-web/meadowlark.js
@@ -6,28 +6,34 @@ app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
+app.use(express.static(__dirname + '/public'));
+
+let fortunes = [
+    "Conquer your fears or they will conquer you.", "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple.",
+];
+
 app.get('/', (req, res) => {
-    res.type('text/plain');
-    res.send('Meadowlark Travel');
+    res.render('home');
 });
 app.get('/about', (req, res) => {
-    res.type('text/plain');
-    res.send('About Meadowlark Travel');
+    let randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+    res.render('about', {fortune: randomFortune});
 });
 
 //定制404页面
 app.use((req, res) => {
-    res.type('text/plain');
     res.status(404);
-    res.send('404 - Not Found');
+    res.render('404');
 });
 
 //定制500页面
 app.use((err, req, res, next) => {
     console.error(err.stack);
-    res.type('text/plain');
     res.status(500);
-    res.send('500 - Server Error');
+    res.render('500');
 });
 
 app.listen(app.get('port'), () => {
