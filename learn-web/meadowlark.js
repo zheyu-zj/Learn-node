5c60729fef00e2279075a200d30528c63ba33d32
zhaojie <zhaojie@example.com>
2018-06-14 19:21:40 +0800

    编写测试

--- learn-web/meadowlark.js
+++ learn-web/meadowlark.js
@@ -6,12 +6,25 @@ app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
-
+app.use(express.static(__dirname + '/public'));
+app.use((req, res, next) => {
+    res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1'
+    next();
+});
 app.get('/', (req, res) => {
     res.render('home');
 });
 app.get('/about', (req, res) => {
-    res.render('about', {fortune: forTunes.getTunes()});
+    res.render('about', {
+        fortune: forTunes.getTunes(),
+        pageTestScript: '/qa/tests-about.js'
+    });
+});
+app.get('/tours/hood-river', (req, res) => {
+    res.render('tours/hood-river');
+});
+app.get('/tours/request-group-rate', (req, res) => {
+    res.render('tours/request-group-rate');
 });
 
 //定制404页面
